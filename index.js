d3255150a550b4a31c8b1081123601859c4265c0
oytundd <oytundd@gmail.com>
2024-02-07 00:56:02 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,45 +1,40 @@
-require('dotenv').config();
-const express = require('express');
-const cors = require('cors');
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
 const app = express();
-const dns = require('dns'); 
+const dns = require("dns");
 // Basic Configuration
 const port = process.env.PORT || 3000;
 
 app.use(cors());
 
-app.use('/public', express.static(`${process.cwd()}/public`));
+app.use("/public", express.static(`${process.cwd()}/public`));
 
-app.get('/', function(req, res) {
-  res.sendFile(process.cwd() + '/views/index.html');
+app.get("/", function (req, res) {
+  res.sendFile(process.cwd() + "/views/index.html");
 });
-app.use(express.json()) 
-app.use(express.urlencoded({ extended: true }))
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 // Your first API endpoint
-app.get('/api/hello', function(req, res) {
-  res.json({ greeting: 'hello API' });
+app.get("/api/hello", function (req, res) {
+  res.json({ greeting: "hello API" });
 });
-const urlArray = []
-app.post('/api/shorturl', (req,res) =>{
-  dns.lookup(req.body.url, (err, address, family) => {
-  if (err) {
-    console.log(`invalid ${req.body.url}`)
-    res.json({error:'invalid url'});
-    
-  }else {
-    if (!req.body.url.includes("http")) {
-      res.json({error: "invalid url"});
-    }else{
-      console.log(`valid ${req.body.url}`)
-      const urlJson = {original_url: req.body.url, short_url: urlArray.length}
-      urlArray.push(urlJson);
-      res.json(urlJson);
-    }
-
+const urlArray = [];
+app.post("/api/shorturl", (req, res) => {
+  if (!req.body.url.includes("http")) {
+    res.json({ error: "invalid url" });
+  } else {
+    console.log(`valid ${req.body.url}`);
+    const urlJson = {
+      original_url: req.body.url,
+      short_url: urlArray.length,
+    };
+    urlArray.push(urlJson);
+    console.log(urlArray);
+    res.json(urlJson);
   }
-})
-}); 
-app.get('/api/shorturl/:index', function(req, res) {
+});
+app.get("/api/shorturl/:index", function (req, res) {
   console.log(`trying to reach /api/shorturl/${req.params.index}`);
   try {
     res.redirect(urlArray[parseInt(req.params.index)].original_url);
@@ -48,6 +43,6 @@ app.get('/api/shorturl/:index', function(req, res) {
   }
 });
 
-app.listen(port, function() {
+app.listen(port, function () {
   console.log(`Listening on port ${port}`);
 });
