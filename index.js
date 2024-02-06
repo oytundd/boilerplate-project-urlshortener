5f0c72d4823aa3408d1132bc9f371b77806ec2fb
oytundd <oytundd@gmail.com>
2024-02-06 23:56:24 +0300

    completed

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const app = express();
-
+const dns = require('dns'); 
 // Basic Configuration
 const port = process.env.PORT || 3000;
 
@@ -13,10 +13,38 @@ app.use('/public', express.static(`${process.cwd()}/public`));
 app.get('/', function(req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
-
+app.use(express.json()) 
+app.use(express.urlencoded({ extended: true }))
 // Your first API endpoint
 app.get('/api/hello', function(req, res) {
   res.json({ greeting: 'hello API' });
+});
+const urlArray = []
+app.post('/api/shorturl', (req,res) =>{
+  // console.log(req.body.url);
+
+  dns.lookup(req.body.url, (err, address, family) => {
+  if (err) {
+    console.log(`invalid ${req.body.url}`)
+    return res.json({error:'invalid url'});
+    // console.error('DNS lookup failed', err);
+    
+  }else {
+    if (!req.body.url.includes("http")) {
+      res.json({error: "invalid url"});
+    }
+    console.log(`valid ${req.body.url}`)
+    const urlJson = {original_url: req.body.url, short_url: urlArray.length}
+    urlArray.push(urlJson);
+    res.json(urlJson);
+  }
+
+  // console.log('IP Address:', address);
+})
+  // url = req.body.url 
+}); 
+app.get('/api/shorturl/:index', function(req, res) {
+  res.redirect(urlArray[parseInt(req.params.index)].original_url);
 });
 
 app.listen(port, function() {
